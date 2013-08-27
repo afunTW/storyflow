1a70c3e4d64bff6674c9246150d7a864b45ccb9a
Ming <endlessproof@gmail.com>
2013-08-27 14:17:21 +0800

    Create web.js

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var tmp = fs.readFileSync('index.html','utf8');
+  tmp.toString('utf8');
+  response.end(tmp); 
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
