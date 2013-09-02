561d2ea779558c0020b72d71fbc73e8beeef3964
endlessproof <endlessproof@gmail.com>
2013-09-02 08:19:41 +0000

    correct the web.js

--- web.js
+++ web.js
@@ -7,26 +7,33 @@ var server = http.createServer(app);
 app.listen(8080);
 console.log("Server Runnung");
 
-app.get('/normalize.css', function(request, response) {
-  var file = fs.readFileSync('normalize.css','utf8');
+app.get('/css/normalize.css', function(request, response) {
+  var file = fs.readFileSync('Final-Project/index/css/normalize.css','utf8');
   response.end(file);
 });
 
-app.get('/storyflow.css', function(request, response) {
-  var file = fs.readFileSync('storyflow.css','utf8');
+app.get('/css/storyflow.css', function(request, response) {
+  var file = fs.readFileSync('Final-Project/index/css/storyflow.css','utf8');
   response.end(file);
 });
 
-app.get('/flow.css', function(request, response) {
-  var file = fs.readFileSync('flow.css','utf8');
+app.get('/css/flow.css', function(request, response) {
+  var file = fs.readFileSync('Final-Project/index/css/flow.css','utf8');
   response.end(file);
 });
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/Final-Project/index/index.html');
   console.log("read index.html");
 });
 
 /*Connect to mongodb*/
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://endlessproof:100321014@ds041218.mongolab.com:41218/heroku_app17770783');
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb://endlessproof:100321014@ds041218.mongolab.com:41218/heroku_app17770783');
+
+/*function*/
+
+app.get('/control.js', function(request, response) {
+  var file = fs.readFileSync('control.js','utf8');
+  response.end(file);
+});
